375e416ddbbd979a41ec12a82813366b15da4e79
steve brazier <steve@meadsteve.dev>
2023-12-05 10:17:37 +0100

    add some code for reading blocks of text

--- src/common.py
+++ src/common.py
@@ -5,3 +5,14 @@ def read_lines(path: str) -> Iterable[str]:
     with open(path, "r") as file:
         while line := file.readline():
             yield line.strip()
+
+
+def blocks_by_blank_line(lines: Iterable[str]) -> Iterable[list[str]]:
+    chunk: list[str] = []
+    for line in lines:
+        if line == "":
+            yield chunk
+            chunk = []
+        else:
+            chunk.append(line)
+    yield chunk
--- src/test_common.py
+++ src/test_common.py
@@ -1,4 +1,4 @@
-from common import read_lines
+from common import read_lines, blocks_by_blank_line
 
 
 def test_the_file_reader_gets_all_lines_when_no_blanks():
@@ -7,3 +7,13 @@ def test_the_file_reader_gets_all_lines_when_no_blanks():
 
 def test_the_file_reader_gets_all_lines_with_blanks():
     assert len(list(read_lines("./src/examples/three_lines_with_a_blank.txt"))) == 3
+
+
+def test_lines_can_be_broken_into_chunks_by_new_lines():
+    lines = read_lines("./src/examples/three_lines_with_a_blank.txt")
+    chunks = list(blocks_by_blank_line(lines))
+
+    assert chunks == [
+        ["line one"],
+        ["line three"],
+    ]
